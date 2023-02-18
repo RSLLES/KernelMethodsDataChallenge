f29416ba2f8912d59e0d5a1e7870bc2f0d1970e9
Louis <louisbf6@gmail.com>
2023-02-18 13:11:57 +0100

    [*] Change linear/gaussian kernel implementation

--- kernels/kernels.py
+++ kernels/kernels.py
@@ -4,12 +4,12 @@ import numpy as np
 class GaussianKernel:
     def __init__(self, sigma) -> None:
         assert sigma > 0, "sigma parameter must be > 0."
-        self.inv_sigma = 1.0 / sigma
+        self.gamma = 1. / (2 * sigma**2)
 
     def __call__(self, x1, x2):
-        return np.exp(-0.5 * np.square((x1 - x2) * self.inv_sigma))
+        return np.exp(-self.gamma * np.linalg.norm(x1 - x2))
 
 
 class LinearKernel:
     def __call__(self, x1, x2):
-        return x1 * x2
+        return np.sum(x1[:, None] * x2[None, :], axis=2)
