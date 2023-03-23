7b5f35b9eef652df6eaf2eb5dbf0440cee605b53
RSLLES <romain.seailles@etu.minesparis.psl.eu>
2023-03-23 12:43:39 +0100

    [+] Add WL depth 3 config

--- configs/wl_depth3.py
+++ configs/wl_depth3.py
@@ -3,5 +3,9 @@ data_directory = "data/"
 x_val = "test_data.pkl"
 x = "training_data.pkl"
 y = "training_labels.pkl"
-prop_test = 0.3
 k_folds_cross_val = 5
+
+### Kernel ###
+from kernels.WL import WeisfeilerLehmanKernel
+
+kernel = WeisfeilerLehmanKernel(depth=3, use_cache=True)
