341348788adab1c9d922114fc25b31c6ab4f5f37
RSLLES <romain.seailles@etu.minesparis.psl.eu>
2023-03-24 12:27:02 +0100

    [+] Add summary of all config results

---
+++ results/merge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+f=./results/README.md
+echo "# Results" > $f
+
+for file in ./results/*.md
+do
+    if [ $file != $f ]
+    then
+        filename=$(basename "$file")
+        filename="${filename%.*}"
+        echo "$filename"
+        echo "## $filename" >> $f
+        cat $file >> $f
+        echo "" >> $f
+    fi
+done
