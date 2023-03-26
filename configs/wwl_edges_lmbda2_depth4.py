f7adcb919239b89ae655761167295a589005f15e
RSLLES <romain.seailles@etu.minesparis.psl.eu>
2023-03-26 12:32:56 +0100

    [+] WWL configs with lambda

---
+++ configs/wwl_edges_lmbda2_depth4.py
@@ -0,0 +1,17 @@
+### Data ###
+data_directory = "data/"
+x_val = "test_data.pkl"
+x = "training_data.pkl"
+y = "training_labels.pkl"
+k_folds_cross_val = 6
+
+### Kernel ###
+from kernels.WWL import WassersteinWeisfeilerLehmanKernel
+
+kernel = WassersteinWeisfeilerLehmanKernel(
+    depth=4, enable_edges_labels=True, lambd=2, use_cache=True
+)
+
+### Save ###
+export_directory = "export/"
+results_directory = "results/"
