d9b10cc037300a8f962f1026b00350b5c6ae13a3
RSLLES <romain.seailles@etu.minesparis.psl.eu>
2023-04-12 00:24:02 +0200

    Add optimize

--- optimization/GWWL.py
+++ optimization/GWWL.py
@@ -1,6 +1,6 @@
 import configs.wl_depth4 as config
 from preprocessing.load import load_data
-from kernels.GWWL import GeneralizedWassersteinWeisfeilerLehmanKernel as Kernel
+from kernels.GWWL import GeneralizedWassersteinWeisfeilerLehmanKernelImport as Kernel
 from run import evaluate_perfs
 import numpy as np
 import pandas as pd
@@ -12,14 +12,13 @@ from bayes_opt.event import Events
 from bayes_opt.util import load_logs
 
 
-def test(depth, lambd, weight):
-    print(f"Params for this run : depth={depth}, lambd={lambd}, weight={weight}")
+def test(log_lambd):
+    print(f"Params for this run : log_lambd={log_lambd}")
     ds, _ = load_data(config=config)
 
     kernel = Kernel(
-        depth=int(depth),
-        lambd=lambd,
-        weight=weight,
+        root="../cost_matrices/",
+        lambd=np.power(10, log_lambd),
         use_cache=True,
     )
     kernel.set_processes(None)
@@ -34,6 +33,7 @@ def test(depth, lambd, weight):
 
     scores = [np.array(score) for score in scores]
     average_scores = sum(scores) / len(scores)
+    print(f"Score = {100*average_scores[-1]:0.3}%")
     return average_scores[-1]
 
 
@@ -43,12 +43,9 @@ def main():
         os.mkdir(path)
 
     json_path = os.path.join(path, "logs.json")
-    results_path = os.path.join(path, "results.csv")
 
     pbounds = {
-        "depth": (3, 9),
-        "lambd": (0.5, 5),
-        "weight": (0.0, 0.5),
+        "log_lambd": (-2, 2),
     }
 
     optimizer = BayesianOptimization(
@@ -64,15 +61,10 @@ def main():
 
     logger = JSONLogger(path=json_path, reset=False)
     optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-    optimizer.set_gp_params(alpha=1e-3)
-<<<<<<< HEAD
-    optimizer.probe(params=[4, 3.0, 0.0])
-=======
-    optimizer.probe(params=[5, 3.0, 0.])
->>>>>>> 4d7ef36 ([~] Final result optimization GWWL)
+    # optimizer.set_gp_params(alpha=1e-3)
     if len(optimizer.space.target) > 0:
         optimizer._gp.fit(optimizer.space.params, optimizer.space.target)
-    optimizer.maximize(n_iter=60, init_points=15)
+    optimizer.maximize(n_iter=25, init_points=8)
 
     df = pd.DataFrame(list(optimizer.res))
     df.index.name = "Iteration"
@@ -83,7 +75,6 @@ def main():
     df = df.sort_values(["target"], ascending=False)
 
     print(df)
-    df.to_csv(results_path)
 
 
 if __name__ == "__main__":
