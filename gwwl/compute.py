ec337d211d19e172437c220d046146f384969fa4
RSLLES <romain.seailles@etu.minesparis.psl.eu>
2023-04-11 20:31:08 +0200

    [+] Change compute

--- gwwl/compute.py
+++ gwwl/compute.py
@@ -22,79 +22,45 @@ def search_for(const, var):
 
 def compute(const, var):
     try:
-        q, Z, granularite, path = const
-        r, c = var
+        Z, path = const
+        i, j = var
 
-        for k, (i, j) in enumerate(zip(r, c)):
-            directory = os.path.join(path, str(i))
-            if not os.path.exists(directory):
-                os.makedirs(directory)
+        directory = os.path.join(path, str(i))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-            file = os.path.join(directory, f"{j}.npy")
-            if not os.path.isfile(file):
-                D = treeEditDistance(Z[i], Z[j])
-                np.save(file, D)
+        file = os.path.join(directory, f"{j}.npy")
+        if not os.path.isfile(file):
+            D = treeEditDistance(Z[i], Z[j])
+            np.save(file, D)
 
-            if k % granularite == 0:
-                q.put(True)
-        return 0.0
     except KeyboardInterrupt:
         print("Caught KeyboardInterrupt, terminating workers")
-        return None
 
 
 def main(path, processes=None, flip=False):
     print("Loading data...")
-    ds, ds_val = load_data(config=config)
+    ds, _ = load_data(config=config)
     X = ds.X
-    n = len(X)
 
     kernel = config.kernel
     kernel.set_processes(processes)
     processes = kernel.processes
-    granularite = max(100, n * (n + 1) // 2 // 2000 // 4)
 
     Z = [kernel.phi(x) for x in tqdm(X, desc="Computing Embedding ...")]
-
-    # Analyse
-    R, C = np.triu_indices(n)
-    with multiprocessing.Pool(processes=processes) as p:
-        m = multiprocessing.Manager()
-        func = partial(search_for, path)
-        res = []
-        for exist in tqdm(
-            p.imap_unordered(func, zip(R, C)),
-            total=len(R),
-            desc="Searching for files...",
-        ):
-            res.append(exist)
-        K = np.array(res)
-
-    mask = ~K
-    R, C = R[mask], C[mask]
 
     if flip:
         R = np.flip(R)
         C = np.flip(C)
-    R, C = np.array_split(R, processes), np.array_split(C, processes)
+    length = R.shape[0]
+
     with multiprocessing.Pool(processes=processes) as p:
-        m = multiprocessing.Manager()
-        q = m.Queue(maxsize=len(R))
-        func = partial(compute, (q, Z, granularite, path))
-        res = p.map_async(func, zip(R, C))
-
-        # Wait
-        with tqdm(total=n * (n + 1) // 2, desc="Inner prods ...") as pbar:
-            last_v, v = 0, 0
-            while not res.ready():
-                v = q.qsize()
-                if v > last_v:
-                    pbar.update(granularite * (v - last_v))
-                    last_v = v
-                sleep(0.01)
-            pbar.update(pbar.total - pbar.n)
-
-    res.wait()
+        func = partial(compute, (Z, path))
+        s = 0
+        for _ in tqdm(
+            p.imap_unordered(func, zip(R, C)), total=length, desc="Inner prod..."
+        ):
+            s += 1
 
 
 import fire
